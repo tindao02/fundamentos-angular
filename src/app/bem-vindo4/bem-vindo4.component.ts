2ee9d39dd954c72d3b5dfd1d4fd262a1d677ae4a
Manoel Tindao <timdao02@hotmail.com>
2020-10-05 19:30:41 -0300

    Usando property binding

--- src/app/bem-vindo4/bem-vindo4.component.ts
+++ src/app/bem-vindo4/bem-vindo4.component.ts
@@ -9,10 +9,16 @@ export class BemVindo4Component
 {
   public nome: string = 'Thiago';
   private idade: number = 10;
+  private nome2: string = '';
 
   public getIdade(): number
   {
     return this.idade;
+  }
+
+  public getNome2(): string
+  {
+    return this.nome2;
   }
 
   public adicionar():void
@@ -24,6 +30,12 @@ export class BemVindo4Component
   {
     //console.log(event.target.value);
     this.nome = event.target.value;
+  }
+
+  public alterarNome2(event: any): void
+  {
+    //console.log(event.target.value);
+    this.nome2 = event.target.value;
   }
 
   public adicionar2(nomeInput: string): void
